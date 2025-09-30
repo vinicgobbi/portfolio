69b7579e5dc23c63c787d80d44f8d6624fc65523
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-09-30 09:33:13 -0300

    feat: adiciona sessões de experiência e contato na home
    Inclui os componentes de experiência e contato na página inicial,
    melhorando a apresentação do perfil e o canal de comunicação.
    Ajusta o espaçamento superior para melhor visualização.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -4,10 +4,12 @@ import { About } from '../about/about';
 import { Stacks } from '../stacks/stacks';
 import { Projects } from '../projects/projects';
 import { Navbar } from '../navbar/navbar';
+import { Contact } from '../contact/contact';
+import { Experience } from '../experience/experience';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar,Header, About, Stacks, Projects],
+  imports: [Navbar,Header, About, Experience, Stacks, Projects, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
